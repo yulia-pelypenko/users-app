97153ee15acba4f18514a58df769b5315193e202
Yulia Pelypenko <yulia.pelypenko.work@gmail.com>
2025-11-03 14:31:47 +0200

    change main button

--- src/modules/common/components/MainButton/index.tsx
+++ src/modules/common/components/MainButton/index.tsx
@@ -1,9 +1,10 @@
 import type { ButtonProps } from "antd";
 import { Button } from "antd";
 import { type FC } from "react";
+import styles from './styles.module.scss'
 
 export const MainButton: FC<ButtonProps> = ({children, ...props}) => {
   return(
-    <Button type="primary" {...props}>{children}</Button>
+    <Button className={styles.button} type="primary" {...props}>{children}</Button>
   )
 }
